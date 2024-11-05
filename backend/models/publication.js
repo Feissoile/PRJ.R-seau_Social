dedd3f871243dbd2777b2edcc38ead5dbd6dd117
Feissoile <feissoileali@gmail.com>
2024-11-05 11:32:11 +0100

    schema

--- backend/models/publication.js
+++ backend/models/publication.js
@@ -1,1 +1,15 @@
 const mongoose = require("mongoose");
+
+const publicationSchema = mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  nickname: String,
+  text: String,
+  picture: String,
+  video: String,
+  date: Date,
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+  });
+
+const Publication = mongoose.model("publications", publicationSchema);
+
+module.exports = Publication;
