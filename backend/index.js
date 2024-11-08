f1858b7d47b35341da404b307b1a7b2db8fe930c
Feissoile <feissoileali@gmail.com>
2024-11-08 14:01:39 +0100

    socket

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { createServer } from 'http';
-import { Server } from 'socket.io';
-import path from 'path';
-import { fileURLToPath } from 'url'; 
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const server = createServer(app);
@@ -11,22 +11,23 @@ const io = new Server(server);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected', socket.id);
-    socket.emit('Bienvenue', 'Bienvenue sur le serveur Socket.io !');
+io.on("connection", (socket) => {
+  console.log("a user connected", socket.id);
+  socket.emit("Bienvenue", "Bienvenue sur le serveur Socket.io !");
 
-    socket.on("disconnect", () => {
-        console.log("Client disconnected:", socket.id);
-    });
-    
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected:", socket.id);
+  });
 });
-
-
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log("server running at http://localhost:3000");
 });
--- frontend/client.js
+++ frontend/client.js
@@ -1,16 +1,9 @@
-// client.js
-
-// Initialiser la connexion avec le serveur de sockets
 const socket = io();
 
-// Vérifier si la connexion est réussie
 socket.on("connect", () => {
-    console.log("Connected to server with ID:", socket.id);
+  console.log("Connected to server with ID:", socket.id);
 });
 
-// Écouter d’autres événements venant du serveur
 socket.on("welcome", (message) => {
-    console.log("Message from server:", message);
+  console.log("Message from server:", message);
 });
-
-// Ajouter d'autres événements selon les fonctionnalités souhaitées
