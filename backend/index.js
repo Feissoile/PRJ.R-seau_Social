f7ddfe3ce7c573e5ae294250a4991f23c9030864
Feissoile <feissoileali@gmail.com>
2024-11-08 10:33:41 +0100

    projet

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,14 @@ import express from 'express';
 import { createServer } from 'http';
 import { Server } from 'socket.io';
 import path from 'path';
+import { fileURLToPath } from 'url'; 
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
