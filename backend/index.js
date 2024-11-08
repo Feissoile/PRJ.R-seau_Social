673b079c6018d06d1f71695239885b84e0f3771a
Feissoile <feissoileali@gmail.com>
2024-11-08 10:17:30 +0100

    projet

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
-import express from ('express');
-import { createServer } from ('http');
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import path from 'path';
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('Nouvelle connexion établie');
+    socket.emit('message', 'Bienvenue sur le serveur Socket.io !');
 });
 
 server.listen(3000, () => {
