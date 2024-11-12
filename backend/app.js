7939710192a219165060f4450dd7c19dd1025ebe
Feissoile <feissoileali@gmail.com>
2024-11-12 14:21:22 +0100

    prj

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+/*require("dotenv").config();
 const userRoutes = require("./routes/user");
 const publicationRoutes = require("./routes/publication");
 
@@ -27,4 +27,39 @@ app.use("/", publicationRouter);
 app.use("/user", userRoutes);
 app.use("/publication", publicationRoutes);
 
-module.exports = app;
+module.exports = app;*/
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
+import cors from "cors";
+
+import indexRouter from "./routes/index.js";
+import userRouter from "./routes/user.js";
+import publicationRouter from "./routes/publication.js";
+
+// Créer l'application express
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(path.resolve(), "public")));
+
+// Routes
+app.use("/", indexRouter);
+app.use("/", userRouter);
+app.use("/", publicationRouter);
+app.use("/user", userRouter);
+app.use("/publication", publicationRouter);
+
+// Exporter l'application en tant que module par défaut
+export default app;
+
