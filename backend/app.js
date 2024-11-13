9de4a8d3cd402e172d2665ef74de4124017d1008
Feissoile <feissoileali@gmail.com>
2024-11-13 14:12:48 +0100

    mercredi

--- backend/app.js
+++ backend/app.js
@@ -1,34 +1,3 @@
-/*require("dotenv").config();
-const userRoutes = require("./routes/user");
-const publicationRoutes = require("./routes/publication");
-
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-
-var indexRouter = require("./routes/index");
-var userRouter = require("./routes/user");
-var publicationRouter = require("./routes/publication");
-var app = express();
-
-const cors = require("cors");
-app.use(cors());
-
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", indexRouter);
-app.use("/", userRouter);
-app.use("/", publicationRouter);
-app.use("/user", userRoutes);
-app.use("/publication", publicationRoutes);
-
-module.exports = app;*/
-
 import dotenv from "dotenv";
 dotenv.config();
 
