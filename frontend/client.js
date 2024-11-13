3d99d26521c4be370d754b790ba3891b2966ba31
Feissoile <feissoileali@gmail.com>
2024-11-13 16:10:38 +0100

    backend

--- frontend/client.js
+++
@@ -1,9 +0,0 @@
-const socket = io();
-
-socket.on("connect", () => {
-  console.log("Connected to server with ID:", socket.id);
-});
-
-socket.on("welcome", (message) => {
-  console.log("Message from server:", message);
-});
